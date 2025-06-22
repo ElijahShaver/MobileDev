99bf190107ba9b7b115eee0cd723d78dd5c5c562
Elijah Shaver <elijahshaver3003@gmail.com>
2025-06-21 23:16:02 -0400

    chapter 12
    leantween is pretty cool i guess

--- Assets/Scripts/MainMenuBehaviour.cs
+++ Assets/Scripts/MainMenuBehaviour.cs
@@ -6,6 +6,8 @@ public class MainMenuBehaviour : MonoBehaviour
 {
     public TMP_Text highScoreText;
 
+    public GameObject menu, otherMenu;
+
     /// <summary>
     /// Will load a new scene upon being called
     /// </summary>
@@ -24,11 +26,66 @@ public class MainMenuBehaviour : MonoBehaviour
 
     private void Start()
     {
-        highScoreText.text = "High Score: " + PlayerPrefs.GetInt("score");
+        GetAndDisplayScore();
+
+        SlideMenuIn(menu);
+        SlideMenuIn(otherMenu);
+
+        /* Unpause the game if needed */
+        Time.timeScale = 1;
     }
 
     private void GetAndDisplayScore()
     {
         highScoreText.text = "High Score: " + PlayerPrefs.GetInt("score").ToString();
+    }
+
+    /// <summary>
+    /// Will move an object from the left side of the screen
+    /// to the center
+    /// </summary>
+    /// <param name="obj">The UI element we would like to
+    /// move</param>
+
+    public void SlideMenuIn(GameObject obj)
+    {
+        obj.SetActive(true);
+        var rt = obj.GetComponent<RectTransform>();
+
+        if (rt)
+        {
+            /* Set the object's position offscreen */
+            var pos = rt.position;
+            pos.x = -Screen.width / 2;
+            rt.position = pos;
+
+            /* Move the object to the center of the screen
+            (x of 0 is centered) */
+            var tween = LeanTween.moveX(rt, 0, 1.5f);
+            tween.setEase(LeanTweenType.easeInOutExpo);
+            tween.setIgnoreTimeScale(true);
+        }
+    }
+
+    /// <summary>
+    /// Will move an object to the right offscreen
+    /// </summary>
+    /// <param name="obj">The UI element we would like to
+    /// move </param>
+    public void SlideMenuOut(GameObject obj)
+    {
+        var rt = obj.GetComponent<RectTransform>();
+
+        if (rt)
+        {
+            var tween = LeanTween.moveX(rt, Screen.width / 2, 0.5f);
+            tween.setEase(LeanTweenType.easeOutQuad);
+            tween.setIgnoreTimeScale(true);
+
+            tween.setOnComplete(() =>
+            {
+                obj.SetActive(false);
+            });
+        }
     }
 }
